5b8c4521f2ac72948b3b23295bb7e6786d9d7e0b
StakNine <staknine@protonmail.com>
2022-06-16 20:41:14 -0700

    Create CalculatorLive with number input

--- lib/temperature_web/live/calculator_live.ex
+++ lib/temperature_web/live/calculator_live.ex
@@ -1,13 +1,35 @@
 defmodule TemperatureWeb.CalculatorLive do
   use Phoenix.LiveView, layout: {TemperatureWeb.LayoutView, "live.html"}
+  use Phoenix.HTML
 
   alias __MODULE__.Component
 
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, temperature: %{value: "0"})}
+  end
+
+  def handle_event("change", %{"temperature" => temperature_params}, socket) do
+    temperature = %{value: temperature_params["value"]}
+
+    {:noreply, assign(socket, :temperature, temperature)}
+  end
+
   def render(assigns) do
     ~H"""
-    <section class="row">
-      <Component.boiling_verdict celsius={100} /> 
-    </section>
+    <div> 
+      <fieldset>
+      <legend>Enter temperature in Celsius:</legend>
+        <.form
+          let={f} 
+          for={:temperature} 
+          id="temperature-form" 
+          phx-change="change" 
+        >
+          <%= number_input f, :value, value: @temperature.value %> 
+        </.form>
+      </fieldset>
+      <Component.boiling_verdict temperature={@temperature} /> 
+    </div>
     """
   end
 end
--- lib/temperature_web/live/calculator_live/components.ex
+++ lib/temperature_web/live/calculator_live/components.ex
@@ -3,11 +3,17 @@ defmodule TemperatureWeb.CalculatorLive.Component do
 
   def boiling_verdict(assigns) do
     ~H"""
-    <%= if @celsius >= 100 do %>
+    <%= if would_boil?(@temperature) do %>
       <p>The water would boil.</p>
     <% else %>
       <p>The water would not boil.</p>
     <% end %>
     """
+  end
+
+  def would_boil?(%{value: ""}), do: false
+
+  def would_boil?(temperature) do
+    String.to_integer(temperature.value) >= 100
   end
 end
